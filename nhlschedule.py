7a506947282c6ebcde0942698d6e9a064ddbc3d0
ThiccCode <mcintojj@gmail.com>
2018-12-07 16:50:07 -0500

    added 4 player functionality

--- nhlschedule.py
+++ nhlschedule.py
@@ -6,10 +6,6 @@ import socket
 import itertools
 import os.path
 from pathlib import Path
-# need a for loop to get the urls for each team schedule
-
-
-url= 'https://www.cbssports.com/nhl/teams/'
 
 team_abreviations = {
 'BOS': 	'boston-bruins',
@@ -44,47 +40,114 @@ team_abreviations = {
 'LV':	'vegas-golden-knights'
 }
 
-def connect_website(website):
-	print('Connecting with '+ website + '...')
-	socket.setdefaulttimeout( 23 )  # timeout in seconds
-	try :
-	    response = urlopen(website)
-	except HTTPError as e:
-		
-	    print ('The server couldn\'t fulfill the request. Reason:', str(e.code) + '\n')
-	except URLError as e:
-	    print ('We failed to reach a server. Reason:', str(e.reason) + '\n')
-	else :
-	    html = response.read()
-	    print ('Connection established!' + '\n')
-	    response.close()
-	    return html
+def compare_teams(player_conflicts_list = None):
+	number_of_teams = int(input("Enter number of teams less than 4: "))
+
+	while 3 < number_of_teams or number_of_teams < 0:
+		number_of_teams = int(input("Invalid number of teams: please re-enter: "))
+	roster = []
+
+	for x in range(number_of_teams):
+		roster_input = input("Enter team: ")
+		roster.append(files[roster_input])
+
+	if player_conflicts_list == None:
+		compared_team = input("Enter team to compare: ")
+		compared_player = files[compared_team]	
+		player_conflicts = schedule_conflicts(roster, compared_player)
+
+	else:
+		player_conflicts = schedule_conflicts(roster, player_conflicts_list)
+
+	return player_conflicts
+
 
 def schedule_conflicts(roster_schedule_list, player_schedule_list):
 
 	conflict_games_list = []
 	available_games_list =[]
 	full_roster_dates = []
+	master_schedule = set(roster_schedule_list[0] + roster_schedule_list[1] + roster_schedule_list[2])
 
-	for player1, player2 in itertools.combinations(roster_schedule_list, 2):
-		for game_date in player1:
-			if game_date in player2:
+	for player1, player2, player3 in itertools.combinations(roster_schedule_list, 3):
+
+		for game_date in master_schedule:
+			if game_date in player1 and game_date in player2:
+				full_roster_dates.append(game_date)
+
+			elif game_date in player2 and game_date in player3:
+				full_roster_dates.append(game_date)
+
+			elif game_date in player1 and game_date in player3:
 				full_roster_dates.append(game_date)
 
 	for compared_player_game_date in player_schedule_list:
 		if compared_player_game_date in full_roster_dates:
+			print("adding " + compared_player_game_date + " to conflict schedule")
 			conflict_games_list.append(compared_player_game_date)
+
 		else:
 			available_games_list.append(compared_player_game_date)
+			print("adding " + compared_player_game_date + " to available schedule")
 
 	print("conflicting games : " + str(conflict_games_list) + '\n')
 	print("total conflicting games: " + str(len(conflict_games_list)) + '\n')
 	print("available games : " + str(available_games_list) + '\n')
 	print("total available games: " + str(len(available_games_list)) + '\n')
+
 	return conflict_games_list
 
+def get_files():
+
+	schedule = None
+	games_dict = {}
+	print("Reading schedule files...")
+	for x in team_abreviations:
+		if os.path.isfile('nhlschedules/' + team_abreviations[x] + '.txt'):
+			try:
+				file = open('nhlschedules/' + team_abreviations[x] + '.txt', "r")
+
+			except OSError as e:
+				print('File ' + team_abreviations[x] + 'could not be found.' + str(e))
+
+		else :
+			try:
+				print('File ' + team_abreviations[x] + 'could not be found, creating new file.')
+				file = open('nhlschedules/' + team_abreviations[x] + '.txt', "w+")
+
+				if schedule is None:
+					schedule = get_schedule()
+				file.write(x + '\n')
+
+				for y in schedule[x]:
+					file.write(y + '\n')
+
+				file.close()
+
+			except OSError as e:
+				print('File ' + team_abreviations[x] + 'could not be created.' + str(e))
+
+		file = open('nhlschedules/' + team_abreviations[x] + '.txt', "r")
+
+		if file.mode == 'r':
+			games_dict.update({file.readline().strip(): parsed_newlines(file.readlines())})
+
+	return games_dict
+
+
+def parsed_newlines(list):
+	parsed_list = []
+
+	for x in list:
+		parsed_list.append(x.strip())
+
+	return parsed_list
+
+
 def get_schedule():
+	url= 'https://www.cbssports.com/nhl/teams/'
 	teams = {}
+
 	for key in team_abreviations:
 
 		team_game_dates = []
@@ -98,70 +161,31 @@ def get_schedule():
 			team_game_dates.append(date.contents[0].strip())
 		
 		teams.update({key: team_game_dates})
+
 	return teams
 
-def get_files():
+def connect_website(website):
+	print('Connecting with '+ website + '...')
+	socket.setdefaulttimeout( 23 )  # timeout in seconds
 
-	schedule = None
-	games_dict = {}
-	print("Reading schedule files...")
-	for x in team_abreviations:
-		if os.path.isfile('nhlschedules/' + team_abreviations[x] + '.txt'):
-			try:
-				file = open('nhlschedules/' + team_abreviations[x] + '.txt', "r")
-			except OSError as e:
-				print('File ' + team_abreviations[x] + 'could not be found.' + str(e))
+	try :
+	    response = urlopen(website)
 
-		else :
-			try:
-				print('File ' + team_abreviations[x] + 'could not be found, creating new file.')
-				file = open('nhlschedules/' + team_abreviations[x] + '.txt', "w+")
-				if schedule is None:
-					schedule = get_schedule()
-				file.write(x + '\n')
-				for y in schedule[x]:
-					file.write(y + '\n')
-				file.close()
-			except OSError as e:
-				print('File ' + team_abreviations[x] + 'could not be created.' + str(e))
-		file = open('nhlschedules/' + team_abreviations[x] + '.txt', "r")
+	except HTTPError as e:
+		
+	    print ('The server couldn\'t fulfill the request. Reason:', str(e.code) + '\n')
 
-		if file.mode == 'r':
-			games_dict.update({file.readline().strip(): parsed_newlines(file.readlines())})
+	except URLError as e:
+	    print ('We failed to reach a server. Reason:', str(e.reason) + '\n')
 
-	return games_dict
-
-def parsed_newlines(list):
-	parsed_list = []
-	for x in list:
-		parsed_list.append(x.strip())
-	return parsed_list
+	else :
+	    html = response.read()
+	    print ('Connection established!' + '\n')
+	    response.close()
+	    
+	    return html
 
 
 files = get_files()
-
-team_one = input("Enter team one: ")
-team_two = input("Enter team two: ")
-compared_team = input("Enter compared team: ")
-
-
-roster = [
-	files[team_one],
-	files[team_two]
-]
-
-compared_player = files[compared_team]
-
-player_conflicts = schedule_conflicts(roster, compared_player)
-
-files = get_files()
-
-team_one2 = input("Enter team one: ")
-team_two2 = input("Enter team two: ")
-
-roster2 = [
-	files[team_one2],
-	files[team_two2]
-	]
-
-schedule_conflicts(roster2, player_conflicts)
+position_one_conflicts = compare_teams()
+position_two_conflicts = compare_teams(position_one_conflicts)
